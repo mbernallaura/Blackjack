27d16b7a9338559a4931cec360de807d20914c97
mbernallaura <mbernallaura@gmail.com>
2022-08-19 17:27:26 -0500

    Darle un evento al boton de pedir carta y este mismo hace que agregue la puntuacion en el HTML

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -5,10 +5,15 @@
     * S = PICAS
 */
 
-
 let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
-const especiales = ['A', 'J', 'Q', 'K']
+const especiales = ['A', 'J', 'Q', 'K'];
+let puntosJugador = 0,
+    puntosMaquina = 0;
+    
+//Referencias del HTML
+const btnPedir = document.querySelector('#btnPedir');
+const puntosHTML = document.querySelectorAll('small');
 
 //Crea una nueva baraja 
 const crearDeck = () =>{
@@ -28,20 +33,43 @@ const crearDeck = () =>{
     console.log(deck);
     return deck;
 }
-
 crearDeck();
 
 //Me permite pedir una carta
-
 const pedirCarta = () =>{ 
-
     if(deck.length === 0){
         throw 'No hay cartas en el deck';
     }
 
-    let cartaSelec = deck.pop();
-    console.log({cartaSelec});
+    const cartaSelec = deck.pop();
     return cartaSelec;
 }
 
-pedirCarta();
+//Nota: Todos los strings en JS, pueden ser trabajados como si fueran un arreglo
+//Nota2: isNaN = Evalua la condicion para saber si no es un numero, y regresa un booleano
+
+const valorCarta = (carta) =>{
+    const valor = carta.substring(0, carta.length -1);
+    /*let puntos = 0;
+    if(isNaN(valor)){
+        puntos = (valor === 'A') ? 11 : 10;
+    }else{
+        console.log('Es un numero');
+        //Se multiplica por 1 para convertir el string numerico en un numero real, para luego poderlo sumar
+        puntos = valor * 1;
+    }*/
+    //Manera Optima de hacer lo anterior
+    return (isNaN(valor)) ?
+           (valor === 'A') ? 11 : 10
+           : valor * 1;
+}
+
+//Nota: Callback: Es una funcion que se esta mandando como argumento
+//Eventos
+btnPedir.addEventListener('click', () => {
+    const carta = pedirCarta();
+    puntosJugador = puntosJugador + valorCarta (carta);
+    puntosHTML[0].innerText = puntosJugador;
+
+    console.log({puntosJugador});
+});
